7ea7b56683f6264a77c2914db2a60f5ea2eeb175
Isaac <iasay@calpoly.edu>
2009-10-30 12:39:03 -0700

    Initial commit

---
+++ iasay_agent.java
@@ -0,0 +1,19 @@
+package Agents.iasay;
+
+import BotEnvironment.SearchBot.*;
+
+public class iasay_agent extends WumpusAgent {
+
+	public iasay_agent() {
+		super();
+		setDeveloperName("Isaac Asay");
+	}
+
+	public void step() {
+
+	}
+
+	public void reset() {
+		super.reset();
+	}
+}
