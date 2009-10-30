5ef655a29d72776c82f489d8f443ab35f43c1082
Isaac <iasay@calpoly.edu>
2009-10-30 14:02:54 -0700

    Began work on the updatePercepts method.

--- iasay_agent.java
+++ iasay_agent.java
@@ -4,12 +4,25 @@ import BotEnvironment.SearchBot.*;
 
 public class iasay_agent extends WumpusAgent {
 
+	private boolean firststep;
+	private MemNode curloc;
+
 	public iasay_agent() {
 		super();
 		setDeveloperName("Isaac Asay");
+		firststep = true;
 	}
 
 	public void step() {
+		// Initially, loop through percepts, updating memory and beliefs, then locate next safe node
+		if (firststep) {
+			MemNode init = new MemNode();
+			init.setVisited(true);
+			init.setSafe(10);
+			updatePercepts(init);
+			firststep = false;
+		}
+	
 		// Continue moving to nearest unvisited safe node
 		/* If move successfull
 				If current node unvisited 
@@ -27,8 +40,29 @@ public class iasay_agent extends WumpusAgent {
 		*/
 	}
 	
+	private void updatePercepts(MemNode mn) {
+		if (nearMinion()) {
+			for (int i = 0; i < 4; i ++) {
+				MemNode n = mn.getNeighbor(i);
+				if (n != null) {
+					if (!n.isVisited()) {
+						if (n.unknown()) {
+							n.setMinion(floor((10 - n.getMinion()) / 2) + n.getMinion());
+							setBelief(i, MAYBE);
+						}
+					}
+				} else {
+					MemNode temp = new MemNode();
+					temp.setNeighbor(Math.abs(i - 2), mn);
+					temp.setMinion(5);
+					setBelief(i, MAYBE);
+				}
+			}
+		}
+	}
+	
 	// Used to locate the next safe unvisited node
-	private Node bfsNextSafe() {
+	private MemNode bfsNextSafe() {
 		
 	}
 	
@@ -89,6 +123,10 @@ private class MemNode {
 		return attributes[WALL];
 	}
 	
+	public MemNode getNeighbor(int dir) {
+		return neighbor[dir];
+	}
+	
 	public void setVisited(boolean val) {
 		visited = val;
 	}
@@ -115,5 +153,9 @@ private class MemNode {
 	
 	public void setWall(int val) {
 		attributes[WALL] = val;	
+	}
+	
+	public void setNeighbor(int dir, MemNode newneighbor) {
+		neighbor[dir] = newneighbor;
 	}
 }
